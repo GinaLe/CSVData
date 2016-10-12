5982d998fbbfc39e8c6301cb314588a7a18e6682
gle771 <gle771@fhs.win.fuhsd.org>
2016-10-12 10:42:17 -0700

    Add method stubs to CSVHelper

--- src/CSVData.java
+++ src/CSVData.java
@@ -5,5 +5,30 @@
  *
  */
 public class CSVData {
+	private String[] columnNames;
+	private double[][] data;
+
+		public CSVData(String[] columnNames, double[][] data) {
+		super();
+		this.columnNames= columnNames;
+		this.data = data;
+	}
+		public static CSVData readCSVFile(String filename, int numLinesToIgnore, String[] columnNames) {
+			return null;
+		}
+		/***
+		 * returns a new CSVData object for a file ignoring lines at the top.
+		 * It uses the first row as the column names.All other data is stored
+		 * as doubles
+		 * @param filename the file to read
+		 * @param numLinesToIgnore number of lines at the top to ignore
+		 * @return a CSVData object for that file
+		 */
+		public static CSVData readCSVFile(String filename, int numLinesToIgnore) {
+			return null;
+		}
+
+	
+
 
 }
