ef2bcdbe72f314502d86d8f3e1f65d621f025ed5
gle771 <gle771@fhs.win.fuhsd.org>
2016-10-12 11:32:28 -0700

    Added more java comments

--- src/CSVData.java
+++ src/CSVData.java
@@ -29,7 +29,7 @@ public class CSVData {
 		}
 		/***
 		 * returns a row specified by the input
-		 * @param rowIndex what row to get
+		 * @param rowIndex row index to get row from
 		 * @return a row specified by the input
 		 */
 		public double[] getRow(int rowIndex) {
@@ -38,42 +38,79 @@ public class CSVData {
 		
 		/***
 		 * returns a column specified by the input
-		 * @param colIndex what column to get
+		 * @param colIndex column index to get column from
 		 * @return a column specified by the input
 		 */
-		
 		public double[] getColumn(int colIndex) {
 			return null;
 		}
 		
+		/***
+		 * returns multiple rows specified by the input 
+		 * @param rowIndexes row indexes to get rows from
+		 * @return rows specified by input
+		 */
 		public static double[][] getRows(int[] rowIndexes) {
 			return null;
 		}
 		
+		/***
+		 * returns multiple columns specified by the input
+		 * @param colIndexes column indexes to get columns from
+		 * @return columns specified by input
+		 */
 		public double[][] getColumns(int[] colIndexes) {
 			return null;
 		}
 		
+		/***
+		 * returns multiple columns specified by the input
+		 * @param colNames column Names to get columns from
+		 * @return columns specified by their titles
+		 */
 		public double[][] getColumns(String[] colNames) {
 			return null;
 		}
+		
+		/***
+		 * sets new values for individual entries
+		 * @param rowIndex row index where new value will be set
+		 * @param colIndex column index where new value will be set
+		 * @param maxValue maximum value of the newm value
+		 */
 		
 		public void setValue(int rowIndex, int colIndex, double maxValue) {
 			
 		}
 		
+		/***
+		 * sets new values in a row
+		 * @param row row that new values will be set in
+		 */
 		public void setRow(int row) {
 			
 		}
 		
+		/***
+		 * sets new values in a column
+		 * @param col column that new values will be set in
+		 */
 		public void setColumn(int col) {
 			
 		}
 		
+		/***
+		 * returns all the column titles
+		 * @return column titles
+		 */
 		public String[] getColumnTitles() {
 			return null;
 		}
 		
+		/***
+		 * saves the current state of the object back into a CSV file 
+		 * @param filename file name that object will be saved into
+		 */
 		public void saveToFile(String filename) {
 			
 		}
